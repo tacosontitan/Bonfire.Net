945ff6249114f256b91851dfa78435511f4bb7b3
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-04-02 15:32:57 -0500

    Created UseStartup<T> extension method for IHostBuilder.

---
+++ src/Retronaut.Net/HostingExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Microsoft.Extensions.Hosting;
+
+/// <summary>
+/// Extension methods for setting up Windows services in an <see cref="IHostBuilder" />.
+/// </summary>
+public static class HostingExtensions
+{
+    /// <summary>
+    /// Specifies the startup type to be used by the host.
+    /// </summary>
+    /// <typeparam name="T">The type containing the startup methods for the application.</typeparam>
+    /// <param name="hostBuilder">The <see cref="IHostBuilder"/> to configure.</param>
+    public static IHostBuilder UseStartup<T>(this IHostBuilder hostBuilder)
+        where T : class, new()
+    {
+        return hostBuilder.ConfigureServices((hostContext, services) =>
+        {
+            var startup = new T();
+            typeof(T).InvokeMember(
+                "ConfigureServices",
+                System.Reflection.BindingFlags.InvokeMethod,
+                null,
+                startup,
+                new object[] { services }
+            );
+        });
+    }
+}
