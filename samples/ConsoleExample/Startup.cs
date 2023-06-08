05c239fb4b9c756c0008624b79b756c155d22691
tacosontitan <jkdavis1691@gmail.com>
2023-06-07 21:42:24 -0500

    Scrapped old project idea.

--- samples/ConsoleExample/Startup.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Extensions.DependencyInjection;
-
-namespace ConsoleExample;
-
-internal sealed class Startup
-{
-    public void ConfigureServices(IServiceCollection services) =>
-        services.AddHostedService<FibonacciService>();
-}
--- samples/StandardizedStartup/Mocking/DefaultStartup.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Extensions.DependencyInjection;
-
-namespace StandardizedStartup.Mocking;
-
-internal class DefaultStartup
-{
-    public virtual void ConfigureServices(IServiceCollection services) =>
-        services.AddHostedService<PrinterService>();
-}
